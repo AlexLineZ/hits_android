70b3f8f374250ec2556d2fe8f9e936f66ce40858
KovkinAleksei <akovkin6@gmail.com>
2023-04-29 20:16:58 +0700

    Добавлены перемещающиеся блоки.

--- app/src/main/java/com/example/hits_android/MainActivity.kt
+++ app/src/main/java/com/example/hits_android/MainActivity.kt
@@ -3,18 +3,26 @@ package com.example.hits_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.hits_android.ui.theme.Hits_androidTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,28 +34,74 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    DefaultPreview()
+                    //Greeting("Android")
+                    Blocks()
                 }
             }
         }
     }
 }
 
+// Группа перетаскиваемых блоков
 @Composable
-fun BoxScope.Greeting(name: String) {
-    Text(text = "Hello $name!", textAlign = TextAlign.Center, modifier = Modifier
-        .fillMaxSize()
-        .align(Alignment.Center))
+fun Blocks() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(25.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        DraggableBox("blok1", Color.Red)
+        DraggableBox("blok2", Color.Yellow)
+        DraggableBox("blok3", Color.Green)
+    }
+}
+
+// Перетаскиваемый блок
+@Composable
+fun DraggableBox(caption: String, bgColor: Color) {
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+
+    Box (
+        Modifier
+            .offset {
+                IntOffset(
+                    x = offsetX.roundToInt(),
+                    y = offsetY.roundToInt()
+                )
+            }
+            .pointerInput(Unit) {
+                detectDragGestures {change, dragAmount ->
+                    change.consumeAllChanges()
+                    offsetX += dragAmount.x
+                    offsetY += dragAmount.y
+                }
+            }
+            .background(color = bgColor)
+            .padding(25.dp)
+            .size(250.dp, 40.dp)
+    ) {
+        Text (
+            modifier = Modifier.align(Alignment.Center),
+            text = caption,
+            fontSize = 30.sp,
+            color = Color(0xFFFFEEFF),
+            textAlign = TextAlign.Right,
+            fontWeight = FontWeight.Bold
+                )
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Hits_androidTheme {
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ){
-            Greeting("YuraLox")
-        }
+        Greeting("Android")
     }
 }
