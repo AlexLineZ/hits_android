51be13414815e1d986ecb8d7cf59d87e8f5f8ce2
sitdi <sitdikov.yuriy@gmail.com>
2023-05-02 23:52:09 +0700

    Added recycleview

--- app/src/main/java/com/example/hits_android/MainScreen.kt
+++ app/src/main/java/com/example/hits_android/MainScreen.kt
@@ -6,15 +6,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -37,16 +45,18 @@ class MainScreen:Screen {
         Hits_androidTheme {
             Surface() {
                 Sandbox()
+                BottomBar()
             }
         }
     }
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun ZoomableComposablePreview() {
     Sandbox()
+    BottomBar()
 }
 
 @Composable
@@ -84,15 +94,10 @@ fun Sandbox() {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DragableBlock(color: Color) {
-
     val haptic = LocalHapticFeedback.current
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember{ mutableStateOf(0f) }
 
-    var offsetX by remember{
-        mutableStateOf(0f)
-    }
-    var offsetY by remember{
-        mutableStateOf(0f)
-    }
     Box(modifier = Modifier
         .offset {
             IntOffset(x=offsetX.roundToInt(),y=offsetY.roundToInt())
@@ -112,7 +117,31 @@ fun DragableBlock(color: Color) {
         Image(
             painter = painterResource(R.drawable.maxresdefault),
             contentDescription = "Papanya",
-            contentScale = ContentScale.FillBounds
+            contentScale = ContentScale.Crop
         )
+    }
+}
+
+@Composable
+fun BottomBar() {
+    Box(
+        Modifier.fillMaxSize()
+    ) {
+        Column(
+            Modifier.align(Alignment.BottomStart)
+        ) {
+            LazyRow () {
+                items(count = 20) {
+                    Image(
+                        painter = painterResource(R.drawable.maxresdefault),
+                        contentDescription = "Papanya",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(100.dp)
+                            .padding(30.dp)
+                    )
+                }
+            }
+        }
     }
 }
