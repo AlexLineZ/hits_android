0bb22d69a61ad4d0b7377403582bdba15caeb73c
sitdi <sitdikov.yuriy@gmail.com>
2023-05-04 21:00:29 +0700

    Added model and blocks

--- app/src/main/java/com/example/hits_android/blocks/MainBlock.kt
+++ app/src/main/java/com/example/hits_android/blocks/MainBlock.kt
@@ -7,12 +7,17 @@ interface Block {
     val blockName: String
     var previousID: Int
     var nextID: Int
+    fun runCodeBlock()
 }
 
 class MainBlock: Block {
     override val blockName = "mainBlock"
     override var previousID = -1
     override var nextID = -1
+
+    override fun runCodeBlock() {
+
+    }
 }
 
 @Composable
