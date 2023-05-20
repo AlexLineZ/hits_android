4a686e374cc304fdd39c10ddbf891c8e23f871e2
KovkinAleksei <akovkin6@gmail.com>
2023-05-20 13:04:14 +0700

    Добавлено удаление блоков из списка блоков.

--- app/src/main/java/com/example/hits_android/MainScreen.kt
+++ app/src/main/java/com/example/hits_android/MainScreen.kt
@@ -60,6 +60,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.hits_android.blocks.blockList
 import com.example.hits_android.model.FlowViewModel
 import com.example.hits_android.model.ReorderListViewModel
 import com.example.hits_android.ui.theme.Hits_androidTheme
@@ -121,6 +122,8 @@ private fun VerticalReorderList(
                                 vm.codeBlocksList = vm.codeBlocksList.toMutableList().apply {
                                     removeIf { it.key == item.key }
                                 }
+
+                                blockList = vm.codeBlocksList.toMutableList()
                             }
                         }
                         true
