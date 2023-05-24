49289b7e3ef4e1575abd8d2dc0056d8063c6413c
sitdi <sitdikov.yuriy@gmail.com>
2023-05-24 12:58:12 +0700

    Фикс дизайна списка функций

--- app/src/main/java/com/example/hits_android/FunctionSelection.kt
+++ app/src/main/java/com/example/hits_android/FunctionSelection.kt
@@ -4,19 +4,18 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,18 +39,19 @@ fun FunctionSelection(vm: ReorderListViewModel) {
             FunctionListItem(vm, item)
         }
         item {
-            Button(
+            Box(
                 modifier = Modifier
                     .fillMaxHeight(0.8f)
                     .clip(shape = RoundedCornerShape(24.dp))
-                    .background(MaterialTheme.colorScheme.primary),
-                onClick = {
-                    vm.addFunction()
+                    .background(MaterialTheme.colorScheme.primary)
+                    .clickable {
+                        vm.addFunction()
 
-                    vm.setCurrentScreenId(vm.functionsList.size - 1)
+                        vm.setCurrentScreenId(vm.functionsList.size - 1)
 
-                    Log.d("a", "${vm.functionsList}")
-                }
+                        Log.d("a", "${vm.functionsList}")
+                    },
+                contentAlignment = Alignment.Center
             ) {
                 Icon(
                     painter = painterResource(R.drawable.ic_add),
@@ -68,37 +68,41 @@ fun FunctionListItem(vm: ReorderListViewModel, item: FunctionClass) {
 
     when (item.id) {
         0 -> {
-            Button(
+            Box(
                 modifier = Modifier
                     .fillMaxHeight(0.8f)
                     .clip(shape = RoundedCornerShape(24.dp))
-                    .background(MaterialTheme.colorScheme.primary),
-                onClick = {
-                    vm.setCurrentScreenId(item.id)
-                }
+                    .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.95f))
+                    .clickable {
+                        vm.setCurrentScreenId(item.id)
+                    },
+                contentAlignment = Alignment.Center
             ) {
                 Text(
                     text = "main",
                     color = MaterialTheme.colorScheme.onPrimary,
                     fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(horizontal = 15.dp)
                 )
             }
         }
 
         else -> {
-            Button(
+            Box(
                 modifier = Modifier
                     .fillMaxHeight(0.8f)
                     .clip(shape = RoundedCornerShape(24.dp))
-                    .background(MaterialTheme.colorScheme.primary),
-                onClick = {
-                    vm.setCurrentScreenId(item.id)
-                }
+                    .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.95f))
+                    .clickable {
+                        vm.setCurrentScreenId(item.id)
+                    },
+                contentAlignment = Alignment.Center
             ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(5.dp)
+                    horizontalArrangement = Arrangement.spacedBy(5.dp),
+                    modifier = Modifier.padding(horizontal = 15.dp)
                 ) {
                     Text(
                         text = "function",
@@ -110,16 +114,9 @@ fun FunctionListItem(vm: ReorderListViewModel, item: FunctionClass) {
                         contentDescription = "closeFunIcon",
                         tint = MaterialTheme.colorScheme.onPrimary,
                         modifier = Modifier
-                            .fillMaxHeight(0.8f)
+                            .fillMaxHeight(0.7f)
                             .clickable {
                                 vm.setCurrentScreenId(0)
-
-                                vm.functionsList.forEach{
-                                    Log.d("a", "${it.id}")
-                                }
-                                Log.d("a", "${item.id}")
-
-
 
                                 vm.functionsList =
                                     vm.functionsList.toMutableList().apply {
