61bcbc7deb952d5784c3737c8015b00d7878ee81
KovkinAleksei <akovkin6@gmail.com>
2023-05-26 19:01:37 +0700

    Добавлена работа с жизненным циклом.

--- app/src/main/java/com/example/hits_android/MainActivity.kt
+++ app/src/main/java/com/example/hits_android/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,7 +18,10 @@ import com.example.hits_android.model.ReorderListViewModelFactory
 import com.example.hits_android.ui.theme.Hits_androidTheme
 import com.example.hits_android.ui.theme.ThemePreference
 
+var isCreated = false
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,7 +31,24 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+
+        setContent {}
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        setContent {
+            Hits_androidTheme {
+                NavigationSystem(this@MainActivity)
+            }
+        }
+    }
 }
+
 
 @Composable
 fun NavigationSystem(context: Context) {
@@ -44,10 +65,15 @@ fun NavigationSystem(context: Context) {
 
     )
 
+    if (isCreated) {
+        MainScreen(navController, viewModel, context)
+    }
+    else {
+        NavHost(navController = navController, startDestination = "start") {
+            composable("start") { StartScreen(navController, viewModel) }
+            composable("main") { MainScreen(navController, viewModel, context) }
+        }
 
-
-    NavHost(navController = navController, startDestination = "start") {
-        composable("start") { StartScreen(navController, viewModel) }
-        composable("main") { MainScreen(navController, viewModel, context) }
+        isCreated = true
     }
 }
